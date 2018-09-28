cb3b6c2385bb28385f17f2ccc47f3d36673a672e
Marc <marcmaters@hotmail.com>
2018-09-28 11:09:19 +0200

    First commit

---
+++ spotitube/src/main/java/nl/han/dea/marc/LogInController.java
@@ -0,0 +1,29 @@
+package nl.han.dea.marc;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/login")
+public class LogInController {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response login(LogInRequestDTO logInRequestDTO) {
+
+        if (logInRequestDTO.getUser().equals("Marc")) {
+            LogInResponseDTO loginresponsedto = new LogInResponseDTO();
+            loginresponsedto.setUser("Marc");
+            loginresponsedto.setToken("adasdasd");
+
+            return Response.ok(loginresponsedto).build();
+        }
+        else {
+            return Response.status(401).build();
+        }
+    }
+}
---
+++ spotitube/src/main/java/nl/han/dea/marc/LogInResponseDTO.java
@@ -0,0 +1,22 @@
+package nl.han.dea.marc;
+
+public class LogInResponseDTO {
+    private String user;
+    private String token;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
